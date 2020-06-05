e015cce1064536d0400499ce2fd9f73d25327698
shaqayeq l <shaghayeghladani@yahoo.com>
2020-06-05 19:53:25 +0000

    Update inverter.v

--- Code/inverter.v
+++ Code/inverter.v
@@ -1,11 +1,11 @@
 `timescale 1ns/1ns
-module inverter #(parameter n=16)(in,out);
+module inverter (in,out);
   
-  input [n-1:0]in;
-  output reg [n-1:0]out;
+  input in;
+  output reg out;
   
   always@(in)begin
-    out=~in+1;
+    out=~in;
   end
 endmodule
   
