95b0b6bb8691f3182ab5f132e9b0dc18e7fb5285
Ghazal <kalhorghazal1378@gmail.com>
2020-07-11 10:21:56 +0000

    Add signals

--- Code/controller.v
+++ Code/controller.v
@@ -7,7 +7,17 @@ module controller
   input                       rst,
   input                       start,
   input                       neg_x,
-  input [ITERATION_WIDTH-1:0] n
+  input [ITERATION_WIDTH-1:0] n,
+  output reg                  done,
+  output reg                  load_x,
+  output reg                  load_y,
+  output reg                  load_z,
+  output reg                  load_d,
+  output reg                  load_d0,
+  output reg [1:0]            sel_x,
+  output reg [1:0]            sel_y,
+  output reg [1:0]            sel_z,
+  output reg                  clear_z
 );
   
   parameter IDLE        = 3'b000;
@@ -18,25 +28,49 @@ module controller
   parameter FINALIZE    = 3'b101;
   
   reg [2:0] next_state;
-  reg [2:0] state;
+  reg [2:0] state;  
+  reg iteration_counter;
   
-  integer iteration_counter;
-  
-  always@(*) begin
+  always@(*) begin   
+    {done, load_x, load_y, load_z, load_d, load_d0, clear_z} = 7'd0;
+    
     case(state)
       IDLE: begin
+        done = 1'd1;
       end
       LOAD: begin
+        clear_z = 1'd1;
+        load_d0 = 1'd1;
+        load_x = 1'd1;
+        load_y = 1'd1;
+        sel_x = 2'd0;
+        sel_y = 2'd0;
       end
       NORMALIZE: begin
+        load_x = 1'd1;
+        load_y = 1'd1;
+        load_z = 1'd1;
+        sel_x = 2'd1;
+        sel_y = 2'd1;
+        sel_z = 2'd0;
       end
       LOOP: begin
+        load_d = 1'd1;
       end
       OPERATE: begin
+        load_x = 1'd1;
+        load_y = 1'd1;
+        load_z = 1'd1;
+        sel_x = 2'd2;
+        sel_y = 2'd2;
+        sel_z = 2'd1;
       end
       FINALIZE: begin
+        load_z = 1'd1;
+        sel_z = 2'd2;
       end
       default: begin
+        {done, load_x, load_y, load_z, load_d, load_d0, clear_z} = 7'd0;
       end
     endcase
   end
@@ -69,10 +103,24 @@ module controller
   end
   
   always@(posedge clk or negedge rst) begin
-    if(~rst)
+    if(~rst) begin
       state <= IDLE;
-    else
+    end
+    else begin
       state <= next_state;
+    end
+  end
+  
+  always@(posedge clk or negedge rst) begin
+    if(~rst) begin
+      iteration_counter <= 0;
+    end
+    else if(state == OPERATE) begin
+      iteration_counter <= iteration_counter + 1;
+    end
+    else begin
+      iteration_counter <= iteration_counter;
+    end
   end
   
 endmodule
