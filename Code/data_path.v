af095b4cb86c426a3e769aab8604d8d1cc73a6a1
Ghazal <kalhorghazal1378@gmail.com>
2020-07-13 06:51:38 +0000

    Update data_path.v

--- Code/data_path.v
+++ Code/data_path.v
@@ -1,29 +1,31 @@
 module data_path
 #(
   parameter WORD_LENGTH = 16,
-  parameter ADDRESS_LENGTH = 4,
-  parameter SHIFT_LENGTH = 5
+  parameter ITERATION_WIDTH = 4
 )
 (
-  input                    clk,
-  input                    rst,          
-  input                    load_x,
-  input                    load_y,
-  input                    load_z,
-  input                    load_d,
-  input                    load_d0,
-  input [1:0]              sel_x,
-  input [1:0]              sel_y,
-  input [1:0]              sel_z,
-  input                    clear_z,
-  input [WORD_LENGTH-1:0]  x_in, 
-  input [WORD_LENGTH-1:0]  y_in, 
-  output [WORD_LENGTH-1:0] z_out
+  input                       clk,
+  input                       rst,          
+  input                       load_x,
+  input                       load_y,
+  input                       load_z,
+  input                       load_d,
+  input                       load_d0,
+  input [1:0]                 sel_x,
+  input [1:0]                 sel_y,
+  input [1:0]                 sel_z,
+  input                       clear_z,
+  input [WORD_LENGTH-1:0]     x_in, 
+  input [WORD_LENGTH-1:0]     y_in, 
+  input [ITERATION_WIDTH-1:0] iteration_counter,
+  output [WORD_LENGTH-1:0]    z_out
 );
 
   wire reg_out_d0;
   wire reg_out_d;
   wire sign_out_y;
+  wire inverted_d;
+  wire inverted_d0;
   
   wire [WORD_LENGTH-1:0] reg_out_x;
   wire [WORD_LENGTH-1:0] reg_out_y;
@@ -33,9 +35,16 @@ module data_path
   
   wire [WORD_LENGTH-1:0] ROM_out;
   
+  wire [WORD_LENGTH-1:0] mult_out;
+  
+  wire [WORD_LENGTH-1:0] inverted_x;
+  
   wire [WORD_LENGTH-1:0] mux_out_x;
   wire [WORD_LENGTH-1:0] mux_out_y;
   wire [WORD_LENGTH-1:0] mux_out_z;
+  
+  wire [WORD_LENGTH-1:0] mux_2to1_out_z;
+  wire [WORD_LENGTH-1:0] mux_2to1_out_y;
   
   wire [WORD_LENGTH-1:0] ALU_out_x;
   wire [WORD_LENGTH-1:0] ALU_out_y;
@@ -43,6 +52,33 @@ module data_path
   
   wire [WORD_LENGTH-1:0] shift_out_x;
   wire [WORD_LENGTH-1:0] shift_out_y;
+  
+  inverter
+  #(
+   .WORD_LENGTH(1)
+  )
+  inverter_inst_d (
+   .in(reg_out_d),
+   .out(inverted_d)
+  );
+  
+  inverter
+  #(
+   .WORD_LENGTH(1)
+  )
+  inverter_inst_d0 (
+   .in(reg_out_d0),
+   .out(inverted_d0)
+  );
+  
+  inverter
+  #(
+   .WORD_LENGTH(WORD_LENGTH)
+  )
+  inverter_inst_x (
+   .in(reg_out_x),
+   .out(inverted_x)
+  );
   
   sign
   #(
@@ -60,6 +96,28 @@ module data_path
   abs_inst (
    .ans(reg_out_y),
    .abs_ans(abs_out_y)
+  ); 
+  
+  mux_2_to_1
+  #(
+   .WORD_LENGTH(WORD_LENGTH)
+  )
+  mux_2_to_1_inst_y (
+   .sel(inverted_d0),
+   .in1(inverted_x), 
+   .in2(reg_out_x), 
+   .out(mux_2to1_out_y)
+  );
+  
+  mux_2_to_1
+  #(
+   .WORD_LENGTH(WORD_LENGTH)
+  )
+  mux_2_to_1_inst_z (
+   .sel(reg_out_d0),
+   .in1(16'b000000000_1000000), // 0.5
+   .in2(16'b111111111_1000000), // -0.5
+   .out(mux_2to1_out_z)
   ); 
   
   mux_3_to_1
@@ -81,7 +139,7 @@ module data_path
   mux_3_to_1_inst_y (
    .sel(sel_y),
    .in1(y_in),
-   .in2(),
+   .in2(mux_2to1_out_y),
    .in3(ALU_out_y),
    .out(mux_out_y)
   ); 
@@ -92,9 +150,9 @@ module data_path
   )
   mux_3_to_1_inst_z (
    .sel(sel_z),
-   .in1(),
+   .in1(mux_2to1_out_z),
    .in2(ALU_out_z),
-   .in3(),
+   .in3(mult_out),
    .out(mux_out_z)
   ); 
   
@@ -164,7 +222,7 @@ module data_path
    .WORD_LENGTH(WORD_LENGTH)
   )
   ALU_inst_x (
-   .ALU_operation(),
+   .ALU_operation(inverted_d),
    .A(reg_out_x),
    .B(shift_out_y),
    .AlU_out(ALU_out_x)
@@ -175,7 +233,7 @@ module data_path
    .WORD_LENGTH(WORD_LENGTH)
   )
   ALU_inst_y (
-   .ALU_operation(),
+   .ALU_operation(reg_out_d),
    .A(reg_out_y),
    .B(shift_out_x),
    .AlU_out(ALU_out_y)
@@ -186,41 +244,51 @@ module data_path
    .WORD_LENGTH(WORD_LENGTH)
   )
   ALU_inst_z (
-   .ALU_operation(),
+   .ALU_operation(inverted_d0),
    .A(reg_out_z),
    .B(ROM_out),
    .AlU_out(ALU_out_z)
   ); 
   
+  multiplier
+  #(
+   .WORD_LENGTH(WORD_LENGTH)
+  )
+  multiplier_inst (
+   .A(reg_out_z),
+   .B(16'b000111001_0100101), // 180/pi
+   .mult_out(mult_out)
+  ); 
+  
   shift_right_var
   #(
    .WORD_LENGTH(WORD_LENGTH),
-   .SHIFT_LENGTH(SHIFT_LENGTH)
+   .SHIFT_LENGTH(ITERATION_WIDTH)
   )
   shift_right_var_inst_x (
    .data_in(reg_out_x),
-   .shift_amount(),
+   .shift_amount(iteration_counter),
    .data_out(shift_out_x)
   ); 
   
   shift_right_var
   #(
    .WORD_LENGTH(WORD_LENGTH),
-   .SHIFT_LENGTH(SHIFT_LENGTH)
+   .SHIFT_LENGTH(ITERATION_WIDTH)
   )
   shift_right_var_inst_y (
    .data_in(reg_out_y),
-   .shift_amount(),
+   .shift_amount(iteration_counter),
    .data_out(shift_out_y)
   ); 
   
   ROM
   #(
    .WORD_LENGTH(WORD_LENGTH),
-   .ADDRESS_LENGTH(ADDRESS_LENGTH)
+   .ADDRESS_LENGTH(ITERATION_WIDTH)
   )
   ROM_inst (
-   .address(),
+   .address(iteration_counter),
    .data(ROM_out)
   );
   
