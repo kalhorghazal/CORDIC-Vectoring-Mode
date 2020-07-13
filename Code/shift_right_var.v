74831a0810ae16b0e141b0f670889c1fb5af458d
Ghazal <kalhorghazal1378@gmail.com>
2020-07-13 06:54:20 +0000

    Replace shift_right_var.v

--- Code/shift_right_var.v
+++ Code/shift_right_var.v
@@ -1,22 +1,17 @@
 module shift_right_var
 #(
     parameter WORD_LENGTH = 16,
-    parameter SHIFT_LENGTH = 5
+    parameter SHIFT_LENGTH = 4
 )
 (
-    input [WORD_LENGTH-1:0]   data_in,
-    input [SHIFT_LENGTH-1:0]  shift_amount,
-    output [WORD_LENGTH-1:0]  data_out
-);
-  
-  reg [WORD_LENGTH-1:0] result;
+    input [WORD_LENGTH-1:0]      data_in,
+    input [SHIFT_LENGTH-1:0]     shift_amount,
+    output reg [WORD_LENGTH-1:0] data_out
+); 
   
   always@(data_in, shift_amount)
   begin
-    result = data_in >> shift_amount;
+    data_out = data_in >> shift_amount;
   end
   
-  assign data_out = result;
-  
 endmodule 
-
