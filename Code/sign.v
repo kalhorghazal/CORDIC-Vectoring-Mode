a704e63c821741c19f44c702bdfd4a5744bd665d
Ghazal <kalhorghazal1378@gmail.com>
2020-07-13 06:54:44 +0000

    Update sign.v

--- Code/sign.v
+++ Code/sign.v
@@ -1,15 +1,13 @@
-`timescale 1ns/1ns
-
 module sign 
 #(
-  parameter NUM_WIDTH = 16
+  parameter WORD_LENGTH = 16
 )
 (
-  input[NUM_WIDTH -1:0] ans,
-  output reg sign_ans
+  input [WORD_LENGTH-1:0] ans,
+  output reg              sign_ans
 );
 always @* begin
-  if (ans[NUM_WIDTH - 1] == 1'b1) begin
+  if (ans[WORD_LENGTH-1] == 1'b1) begin
     sign_ans = 1'b1;
   end
   else begin
