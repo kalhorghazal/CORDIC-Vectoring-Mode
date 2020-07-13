53027cb1b8df058e67c289dd7a8175a8cdedc70f
Ghazal <kalhorghazal1378@gmail.com>
2020-07-13 06:52:47 +0000

    Update mux_2_to_1.v

--- Code/mux_2_to_1.v
+++ Code/mux_2_to_1.v
@@ -1,12 +1,21 @@
-`timescale 1ns/1ns
-module mux_2_to_1 #(parameter n=16)(in1,in2,sel,out);
-  input [n-1:0]in1;
-  input [n-1:0]in2;
-  input sel;
-  output reg [n-1:0]out;
+module mux_2_to_1 
+#(
+  parameter WORD_LENGTH = 16
+)
+(
+  input                        sel,
+  input [WORD_LENGTH-1:0]      in1,
+  input [WORD_LENGTH-1:0]      in2,
+  output reg [WORD_LENGTH-1:0] out
+);
   
-  always@(in1,in2,sel)begin
-    out=sel?in2:in1;
+  always@(sel or in1 or in2) begin
+    out = 0;
+    case(sel)
+      1'd0: out = in1;
+      1'd1: out = in2;
+      default: out = 0;
+    endcase
   end
+  
 endmodule
-
