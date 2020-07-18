65b5527987234e9a081febe18113c2b96921f59f
Ghazal <kalhorghazal1378@gmail.com>
2020-07-18 10:59:26 +0000

    Upload New File

---
+++ ModelSim/interface.v
@@ -0,0 +1,34 @@
+`include "defines.v"
+
+module interface
+(
+  input                            clk, 
+  input                            rst,
+  input                            start,
+  output                           done,
+  input signed [`WORD_WIDTH-1:0]   x_in,
+  input signed [`WORD_WIDTH-1:0]   y_in,
+  output signed [`PHASE_WIDTH-1:0] z_out
+);
+
+  cordic cordic_inst (
+   .clk(clk),
+   .rst(rst),
+   .x_in(x_in),
+   .y_in(y_in),
+   .z_out(z_out)
+  );
+  
+  control_unit
+  #(
+   .ITERATION_WIDTH(`ITERATION_WIDTH),
+   .ITERATIONS(`ITERATIONS)
+  )
+  control_unit_inst (
+   .clk(clk),
+   .rst(rst),
+   .start(start),
+   .done(done)
+  );
+
+endmodule
