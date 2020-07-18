1755f9355c4c5d60db4c09ba185a9b6a5a84e0f7
Ghazal <kalhorghazal1378@gmail.com>
2020-07-18 11:00:04 +0000

    Upload New File

---
+++ ModelSim/register.v
@@ -0,0 +1,21 @@
+ module register
+#(
+  parameter WORD_WIDTH = 16
+)  
+(
+  input                               clk,
+  input                               rst,
+  input signed [WORD_WIDTH-1:0]      data_in,
+  output reg signed [WORD_WIDTH-1:0] data_out
+);
+  
+always @(posedge clk or negedge rst) begin
+  if(~rst) begin
+    data_out <= 0;
+  end  
+  else begin
+    data_out <= data_in;
+  end    
+ end
+       
+endmodule
