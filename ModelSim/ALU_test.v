01363a15d6ab20a71a6b21511baad02459908dd5
Ghazal <kalhorghazal1378@gmail.com>
2020-07-23 22:20:41 +0000

    Refactor

--- ModelSim/ALU_test.v
+++ ModelSim/ALU_test.v
@@ -1,32 +1,81 @@
-module ALU_test
+module ALU_test;
+  
+  parameter WORD_WIDTH = 16;
+  
+  parameter ADD = 2'd0; 
+  parameter SUB = 2'd1;
+  parameter NOP = 2'd2;
+  
+  reg [1:0]                     ALU_operation;
+  reg signed [WORD_WIDTH-1:0]   A;
+  reg signed [WORD_WIDTH-1:0]   B;
+  wire signed [WORD_WIDTH-1:0]  ALU_out;
+  
+  reg [8*4:1] operator;
+  
+  //-----instance of ALU module
+  ALU 
   #(
-    parameter WORD_WIDTH = 16
-  )
-  (
-  );
-  
-  parameter ADD = 1'd0; 
-  parameter SUB = 1'd1;
-  
-  reg                           ALU_OPERATION;
-  reg signed [WORD_WIDTH-1:0]   a;
-  reg signed [WORD_WIDTH-1:0]   b;
-  wire signed [WORD_WIDTH-1:0]  ALU_OUT;
-  
-  ALU op(.ALU_operation(ALU_OPERATION),.A(a),.B(b),.AlU_out(ALU_OUT));
+    .WORD_WIDTH(WORD_WIDTH)
+  ) ALU_inst (
+    .ALU_operation(ALU_operation),
+    .A(A),
+    .B(B),
+    .ALU_out(ALU_out)
+    );
   
   initial begin
-    a=15;
-    b=4;
-    ALU_OPERATION=SUB;
-    #200;
-    a=487;
-    b=49;
-    ALU_OPERATION=ADD;
-    #200;
-    $stop;
+     ALU_operation = ADD;
+     A = {WORD_WIDTH{1'b0}}; 
+     B = {WORD_WIDTH{1'b0}};
+     operator = "+";
+     #10
+     ALU_operation = ADD;
+     A = 16'd16;
+     B = 16'd32;
+     operator = "+";
+     #10
+     ALU_operation = ADD;
+     A = 16'd21;
+     B = 16'd18;
+     operator = "+";
+     #10
+     ALU_operation = NOP;
+     A = {WORD_WIDTH{1'b1}};
+     B = 16'd1;
+     operator = "?";
+     #10
+     ALU_operation = NOP;
+     A = 16'd128;
+     B = 16'd17;
+     operator = "?";
+     #10
+     ALU_operation = NOP;
+     A = 16'd64;
+     B = 16'd24;
+     operator = "?";
+     #10
+     ALU_operation = SUB;
+     A = 16'd20;
+     B = 16'd20;
+     operator = "-";
+     #10
+     ALU_operation = SUB;
+     A = 16'd72;
+     B = 16'd25;
+     operator = "-";
+     #10
+     ALU_operation = SUB;
+     A = 16'd45;
+     B = 16'd90;
+     operator = "-";
+     #20 $stop;
   end
+  
+  initial begin
+    $display("ALU WIDTH = %0d", WORD_WIDTH);
+    $monitor("@%3tns: %0d %0s %0b = %0b", 
+      $time, A, operator, B, AlU_out);
+    end
+  
 endmodule
-    
-
-
