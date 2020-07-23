265b0841667a3f37cd68942e0deaf50411f1807f
Ghazal <kalhorghazal1378@gmail.com>
2020-07-23 14:26:05 +0000

    Add File operation

--- ModelSim/cordic_test.v
+++ ModelSim/cordic_test.v
@@ -3,8 +3,14 @@
 `include "defines.v"
 
 module cordic_test;
+  
+integer file_x, file_y, file_z;
+integer scan_x, scan_y;
 
 parameter clock_period = 2;
+parameter TEST_CASES = 100;
+
+localparam sf = 2.0**-7.0;  //scaling factor is 2^-7
 
 reg                            clk;
 reg                            rst;
@@ -13,13 +19,6 @@ wire                           done;
 reg signed [`WORD_WIDTH-1:0]   x_in;
 reg signed [`WORD_WIDTH-1:0]   y_in;
 wire [`PHASE_WIDTH-1:0] z_out;
-
-//generating clock for the test bench
-initial
-   begin
-      clk = 0;
-      forever clk = #(clock_period/2)  ~clk;
-   end
 
 //-----instance of interface module
 interface
@@ -33,66 +32,61 @@ interface
   .z_out(z_out)
 )
 ;
-localparam sf = 2.0**-7.0;  // Q4.4 scaling factor is 2^-7
 
+//generating clock for the test bench
 initial
- begin
-     x_in = 16'b10; // (2, 1) -> real_theta = 26.56 , result = 18.75
-     y_in = 16'b1;
-     start  = 1;
-     rst = 0;
-     # clock_period;
-     rst = 1;
-     start = 0;
-     # (100*clock_period);
-     x_in = 16'b11; // (3, 4) -> real_theta = 53.13 , result = 57.5
-     y_in = 16'b100;
-     start = 1;
-     rst = 0;
-     # clock_period;
-     rst = 1;
-     # (100*clock_period);
-     x_in = 16'b100; // (4, 3) -> real_theta = 36.87 , result = 32.75
-     y_in = 16'b11;
-     start = 1;
-     rst = 0;
-     # clock_period;
-     rst = 1;
-     # (100*clock_period);
-     x_in = 16'b1111111111111111; // (-1, -1)  -> real_theta = 225, result = 223.5
-     y_in = 16'b1111111111111111;
-     start = 1;
-     rst = 0;
-     # clock_period;
-     rst = 1;
-     # (100*clock_period);
-     x_in = 16'b0000000000000001; // (1, -1) -> real_theta = 315, result = 313.5
-     y_in = 16'b1111111111111111;
-     start = 1;
-     rst = 0;
-     # clock_period;
-     rst = 1;
-     # (100*clock_period);
-     x_in = 16'b0000000000000001; // (1, 1) -> real_theta = 45, result = 43.5
-     y_in = 16'b0000000000000001;
-     start = 1;
-     rst = 0;
-     # clock_period;
-     rst = 1;
-     # (100*clock_period);
-     x_in = 16'b1111111111111111; // (-1, 1) -> real_theta = 135, result = 133.4
-     y_in = 16'b0000000000000001;
-     start = 1;
-     rst = 0;
-     # clock_period;
-     rst = 1;
-     # (100*clock_period);
-     $stop;
+   begin
+      clk = 0;
+      forever clk = #(clock_period/2)  ~clk;
+   end
+
+initial begin
+    file_x = $fopen("x_values.txt", "r");
+    if (file_x == 1) begin
+      $display("file_x handle was NULL");
+      $finish;
+    end
   end
   
-  initial begin
-    $monitor("@%3tns: done = %0d, arctan(%0d/%0d) = %0f", 
-      $time, done, y_in, x_in, $itor(z_out)*sf);
+initial begin
+  file_y = $fopen("y_values.txt", "r");
+  if (file_y == 1) begin
+    $display("file_y handle was NULL");
+    $finish;
   end
+end
+
+initial begin
+  file_z = $fopen("verilog_phase.txt", "w");
+  if (file_z == 1) begin
+    $display("file_y handle was NULL");
+    $finish;
+  end
+end
+
+initial
+   begin
+      repeat(TEST_CASES)
+        begin
+          if(!$feof(file_x))
+              scan_x = $fscanf(file_x, "%b\n", x_in);
+            
+          if(!$feof(file_y))
+              scan_y = $fscanf(file_y, "%b\n", y_in);
+              
+          start  = 1;
+          rst = 0;
+          # clock_period;
+          rst = 1;
+          start = 0;
+          # (100*clock_period);
+          
+          $fdisplay(file_z, "%0f", $itor(z_out)*sf);
+              
+          $display("@%3tns: arctan(%0d/%0d) = %0f", 
+              $time, y_in, x_in, $itor(z_out)*sf);
+        end
+        $stop;
+   end
     
 endmodule
