fe1c82308854064835dcdcda9b144e63027d5bf8
Ghazal <kalhorghazal1378@gmail.com>
2020-07-23 10:41:37 +0000

    Replace cordic.py

--- GoldenModel/cordic.py
+++ GoldenModel/cordic.py
@@ -3,23 +3,6 @@ import math
 def ROM_lookup(iteration):
     return math.degrees(math.atan(2**(-1*iteration)))
 
-ROM_lookup = {0 : 45.000000,\
-        1 : 22.500000,\
-        2 : 11.250000,\
-        3 : 5.6250000,\
-        4 : 2.8125000,\
-        5 : 1.4062500,\
-        6 : 0.7031250,\
-        7 : 0.3515625,\
-        8 : 0.1718750,\
-        9 : 0.0859375,\
-        10: 0.0390625,\
-        11: 0.0156250,\
-        12: 0.0078125,\
-        13: 0.0000000,\
-        14: 0.0000000,\
-        15: 0.0000000} 
-
 def vector_mode(x, y, iterations):
     
     x_val_list = []
@@ -29,21 +12,20 @@ def vector_mode(x, y, iterations):
 
     i = 0;                # Keeps count on number of iterations
     
-    current_x = x         # Value of X on ith iteration 
-    current_y = y         # Value of Y on ith iteration
+    current_x = abs(x)         # Value of X on ith iteration 
+    current_y = abs(y)         # Value of Y on ith iteration
     current_z = 0
-
-            
+    
     if (iterations > 0):
         while (i < iterations):
             if (current_y > 0):
                 next_x = current_x + current_y * (2**(-1*i))
                 next_y = current_y - current_x * 2**(-1*i)
-                next_z = current_z + ROM_lookup[i]
+                next_z = current_z + ROM_lookup(i)
             else:
                 next_x = current_x - current_y * (2**(-1*i))
                 next_y = current_y + current_x * 2**(-1*i)
-                next_z = current_z - ROM_lookup[i]
+                next_z = current_z - ROM_lookup(i)
 
             current_x = next_x
             current_y = next_y
@@ -57,10 +39,13 @@ def vector_mode(x, y, iterations):
 
             i = i+1
 
+    if (x < 0 and y < 0):
+        current_z += 180
+
+    if (x < 0 and y > 0):
+        current_z += 90
+
+    if (x > 0 and y < 0):
+        current_z += 270
+
     return current_z
-
-
-#x_in = 3
-#y_in = 4
-#iterations = 16
-#print('arctan({}/{}) = {}'.format(y_in, x_in, vector_mode(y_in, x_in, iterations))) 
