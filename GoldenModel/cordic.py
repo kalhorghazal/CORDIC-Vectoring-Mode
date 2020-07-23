df1eb68497df32ddad7ded667bafc1348c174ba8
Ghazal <kalhorghazal1378@gmail.com>
2020-07-23 17:53:00 +0000

    Fix bugs

--- GoldenModel/cordic.py
+++ GoldenModel/cordic.py
@@ -22,6 +22,10 @@ def vector_mode(x, y, iterations):
                 next_x = current_x + current_y * (2**(-1*i))
                 next_y = current_y - current_x * 2**(-1*i)
                 next_z = current_z + ROM_lookup(i)
+            else if (current_y == 0):
+                next_x = current_x
+                next_y = current_y
+                next_z = current_z
             else:
                 next_x = current_x - current_y * (2**(-1*i))
                 next_y = current_y + current_x * 2**(-1*i)
