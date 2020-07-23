2b626bc01a29534a05acce843bf30bd0c885f364
Ghazal <kalhorghazal1378@gmail.com>
2020-07-23 18:53:22 +0000

    Replace cordic.py

--- GoldenModel/cordic.py
+++ GoldenModel/cordic.py
@@ -1,7 +1,20 @@
-import math
+ROM_lookup = {0 : 45.000000,\
+        1 : 22.500000,\
+        2 : 11.250000,\
+        3 : 5.6250000,\
+        4 : 2.8125000,\
+        5 : 1.4062500,\
+        6 : 0.7031250,\
+        7 : 0.3515625,\
+        8 : 0.1718750,\
+        9 : 0.0859375,\
+        10: 0.0390625,\
+        11: 0.0156250,\
+        12: 0.0078125,\
+        13: 0.0000000,\
+        14: 0.0000000,\
+        15: 0.0000000} 
 
-def ROM_lookup(iteration):
-    return math.degrees(math.atan(2**(-1*iteration)))
 
 def vector_mode(x, y, iterations):
     
@@ -19,17 +32,17 @@ def vector_mode(x, y, iterations):
     if (iterations > 0):
         while (i < iterations):
             if (current_y > 0):
-                next_x = current_x + current_y * (2**(-1*i))
-                next_y = current_y - current_x * 2**(-1*i)
-                next_z = current_z + ROM_lookup(i)
+                next_x = current_x + current_y // (2**i)
+                next_y = current_y - current_x // (2**i)
+                next_z = current_z + ROM_lookup[i]
             elif (current_y == 0):
                 next_x = current_x
                 next_y = current_y
                 next_z = current_z
             else:
-                next_x = current_x - current_y * (2**(-1*i))
-                next_y = current_y + current_x * 2**(-1*i)
-                next_z = current_z - ROM_lookup(i)
+                next_x = current_x - current_y // (2**i)
+                next_y = current_y + current_x // (2**i)
+                next_z = current_z - ROM_lookup[i]
 
             current_x = next_x
             current_y = next_y
