eef4a8bc7fb0597df40737277357b678db550529
Ghazal <kalhorghazal1378@gmail.com>
2020-07-25 15:38:03 +0000

    Add header

--- ModelSim/shift_right_var.v
+++ ModelSim/shift_right_var.v
@@ -1,18 +1,22 @@
+`include "settings.h"  
+
 module shift_right_var 
 #(
-  parameter WORD_WIDTH = 16,
-  parameter SHIFT_WIDTH = 4
+  parameter WORD_WIDTH = `WORD_WIDTH,
+  parameter SHIFT_WIDTH = `SHIFT_WIDTH
 )
 (
   input signed [WORD_WIDTH-1:0]      data_in,
   input [SHIFT_WIDTH-1:0]            shift_amount,
   output reg signed [WORD_WIDTH-1:0] data_out 
 );
-  integer i;
+
+ integer i;
   always @(*) begin
     data_out = data_in;
     for(i=0; i < shift_amount; i=i+1) begin
       data_out = (data_out >> 1) | (data_in[WORD_WIDTH-1] << (WORD_WIDTH-1));
     end
   end
-endmodule
+
+endmodule 
